3f661af1cf0ee8c4ece30cb1898750bc11f31d02
YueDayu <yuedayu13@gmail.com>
2014-11-28 10:39:58 +0800

    实现登陆功能。实现简单的socket

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 //number of two team
 var blue = 0;
@@ -22,7 +24,7 @@ var userId;
 var onlineUsers = {};
 
 //read the name information
-fs.readFile('password.json', function(err, data){
+fs.readFile('password.json', function (err, data) {
     if (err) {
         console.log('Can not read the file.');
     } else {
@@ -30,27 +32,77 @@ fs.readFile('password.json', function(err, data){
     }
 });
 
-app.get('/', function(req, res){
-    fs.readFile('index.html', function(err, data) {
+app.get('/', function (req, res) {
+    if (req.cookies.team == 'red' || req.cookies.team == 'blue') {
+        fs.readFile('index.html', function (err, data) {
+            if (err) {
+                console.log("can not open the file");
+            } else {
+                res.send(data.toString());
+            }
+        });
+    } else if (req.cookies.team == 'admin'){
+        res.redirect('/admin');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/login', function (req, res) {
+    if (!req.cookies.team) {
+        fs.readFile('login.html', function (err, data) {
+            if (err) {
+                console.log("can not open the file");
+            } else {
+                res.send(data.toString());
+            }
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/admin', function (req, res) {
+    if (req.cookies.team == 'admin') {
+        fs.readFile('admin.html', function (err, data) {
+            if (err) {
+                console.log("can not open the file");
+            } else {
+                res.send(data.toString());
+            }
+        });
+    } else {
+        res.redirect('/error');
+    }
+});
+
+app.get('*', function (req, res) {
+    fs.readFile('404.html', function (err, data) {
         if (err) {
             console.log("can not open the file");
         } else {
-            res.cookie('test', 'hehe');
             res.send(data.toString());
         }
     });
 });
 
-app.post('/',  function(req, res) {
+app.post('/login', function (req, res) {
     var username = req.param('username', null);
     var password = req.param('password', null);
     console.log(req.body);
     console.log(username + password);
     var issuccess = false;
+    if (userId['admin'].username == username && userId['admin'].password == password) {
+        res.cookie('username', username);
+        res.cookie('team', 'admin');
+        res.redirect('/admin');
+        return;
+    }
     for (x in userId['redTeam']) {
         if (userId['redTeam'][x].username == username && userId['redTeam'][x].password == password) {
             res.cookie('username', username);
             res.cookie('team', 'red');
+            red++;
             issuccess = true;
         }
     }
@@ -59,21 +111,37 @@ app.post('/',  function(req, res) {
             if (userId['blueTeam'][x].username == username && userId['blueTeam'][x].password == password) {
                 res.cookie('username', username);
                 res.cookie('team', 'blue');
+                blue++;
                 issuccess = true;
             }
         }
     }
     if (issuccess == true) {
-        console.log(username + ' login');
+        res.redirect('/');
     } else {
-        console.log('false');
+        res.redirect('/error');
     }
 });
 
-http.listen(8080, function(){
+http.listen(80, function () {
     console.log('Server Start');
 });
 
-io.on('connection', function(socket){
-    console.log("connect");
+var currentStatus = -1; //waiting for admin: -1; waiting for starting: 0; waiting for answer: 1;
+var answerFlag = false; //no answer: false; have a answer: true;
+
+io.on('connection', function (socket) {
+    socket.on('login', function(obj){
+        var userName = obj.username;
+        var Team = obj.team;
+        socket.name = {username: userName, team : Team};
+        if (userName == 'admin') {
+            console.log('admin login!');
+            currentStatus = 0;
+        } else if(Team == 'red') {
+            console.log('red team member login!');
+        } else {
+            console.log('blue team member login!');
+        }
+    });
 });
